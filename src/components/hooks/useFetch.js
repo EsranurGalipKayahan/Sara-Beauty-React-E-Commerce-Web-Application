773ccc0eebaae073304998dfd3d80ee3f08f8bef
Esra Galip Kayahan <esra.galipkayahan@arup.com>
2022-07-22 14:39:17 +0200

    method was added to fetch

--- src/components/hooks/useFetch.js
+++ src/components/hooks/useFetch.js
@@ -16,7 +16,7 @@ export const useFetch = (url) => {
 
   const fetchData = async () => {
     try {
-      const response = await fetch(url);
+      const response = await fetch(url,{method : 'GET'});
       const resData = await response.json();
 
       setData(resData);
