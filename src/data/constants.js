3d6fd2f2edd12d6e5ee147675fab08d1dbb6cceb
Esra Galip Kayahan <esra.galipkayahan@arup.com>
2022-07-22 16:11:16 +0200

    deploy adjustment

--- src/data/constants.js
+++ src/data/constants.js
@@ -4,7 +4,7 @@ export const MESSAGES = {
   FREE_SHIP_PRICE_LIMIT: "over €50",
 };
 export const GEO_URL = `https://api.freegeoip.app/json/?apikey=${process.env.REACT_APP_GEO_API_KEY}`;
-export const PRODUCT_URL = `http://makeup-api.herokuapp.com/api/v1/products`;
+export const PRODUCT_URL = `https://makeup-api.herokuapp.com/api/v1/products`;
 export const COLLECTION_URL =
-  "http://makeup-api.herokuapp.com/api/v1/products.json?product_type=";
+  "https://makeup-api.herokuapp.com/api/v1/products.json?product_type=";
 export const TRACKING_URL = "https://www.dhl.com/nl-en/home/tracking.html";
